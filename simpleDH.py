436c0c2f40ba5e3d43870fc9a111518fa7370e93
mark <mark@ulaptop>
2015-10-14 11:08:13 -0600

    Rename, comment and update simpleDH for Python 2/3 compatibility

--- simpleDH.py
+++ simpleDH.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 """
 A simple Diffie-Hellman example in python
-
 """
+
 from random import getrandbits
 
 g = 2
 prime = 7919
 bits = 32
 
+# Generate Alice's secret and public keys (a,A)
 a = getrandbits(bits)
 A = pow(g, a, prime)
 
+# Generate Bob's secret and public keys (b,B)
 b = getrandbits(bits)
 B = pow(g, b, prime)
 
+# Generate the shared secrets
 s1 = pow(A, b, prime)
 s2 = pow(B, a, prime)
 
 if(s1 == s2):
-	print "Shared secrets match: ", s1
+    print("Shared secrets match: ", s1)
